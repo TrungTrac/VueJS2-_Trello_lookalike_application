4ace2cc4538a8c3451b8ad46767d7113553c20fd
TrungTrac <152832896+TrungTrac@users.noreply.github.com>
2024-04-05 09:05:39 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="header">
+    <h2 class="logo" @click="$router.push('/home')">
+      <span class="text-orange"></span><span class="font-weight-light">Trolle</span>
+    </h2>
+
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/utils/exampleTasks.js
@@ -0,0 +1,36 @@
+export default {
+  todo: {
+    name: 'todo',
+    title: 'To do ',
+    tasks: [
+      {
+        text: 'task 1',
+      },
+      {
+        text: 'task 2',
+      },
+    ],
+  },
+
+  doing: {
+    name: 'doing',
+    title: 'Doing ',
+    tasks: [
+      {
+        text: 'task 3',
+      },
+    ],
+  },
+
+  blocked: {
+    name: 'blocked',
+    title: 'Blocked ',
+    tasks: [],
+  },
+
+  done: {
+    name: 'done',
+    title: 'Done ',
+    tasks: [],
+  },
+};
